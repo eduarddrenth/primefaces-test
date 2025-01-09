9ec3ebf2a69835b95d4b5e6832578859bed1f882
eduarddrenth <eduarddrenth@gmail.com>
2025-01-09 19:35:06 +0100

    monkey patch works

--- src/main/webapp/resources/test.js
+++ src/main/webapp/resources/test.js
@@ -1,1 +1,13 @@
 /* Place any Javascript here you want loaded in test.xhtml */
+if (PrimeFaces.widget.DataTable) {
+    PrimeFaces.widget.DataTable.prototype.disableCellEditors = function(element) {
+        if (element) {
+            $(element).find(":input:enabled").attr('disabled', 'disabled');
+        } else {
+            $(".ui-cell-editor-input :input:enabled").attr('disabled', 'disabled').attr("data-disabled-by-editor", "true");
+            //#13159: re-enable for all rows that are rowEditing="true"
+            this.enableCellEditors($('.ui-row-editing'));
+
+        }
+    }
+}
